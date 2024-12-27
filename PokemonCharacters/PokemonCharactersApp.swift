68cf12f8ed2560700157de4d16826cb0f89e46f6
Lynn Do <do24l@mtholyoke.edu>
2024-12-26 21:57:40 -0800

    Initial Commit

---
+++ PokemonCharacters/PokemonCharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonCharactersApp.swift
+//  PokemonCharacters
+//
+//  Created by Do Linh on 12/26/24.
+//
+
+import SwiftUI
+
+@main
+struct PokemonCharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
